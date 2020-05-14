5f67a2f4177e679cab3dc3978d4cfe2f0a38d6c6
Cloud1PH <59385067+Cloud1PH@users.noreply.github.com>
2020-05-15 02:55:04 +0800

    Create single_node-tainted.sh

---
+++ CRI-O/1.15/single_node-tainted.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#K8s prereq for crio https://kubernetes.io/docs/setup/production-environment/container-runtimes/#cri-o
+modprobe overlay
+modprobe br_netfilter
+
+# Set up required sysctl params, these persist across reboots.
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+
+#Add repo
+add-apt-repository -y ppa:projectatomic/ppa
+apt update
+
+#Install cri-o 1.15
+apt-get install -y cri-o-1.15
+
+#Change cgroup manager from systemd to cgroupfs to avoid issue https://github.com/cri-o/cri-o/issues/896 and add docker.io as default repo
+#Download crio.conf from our phdsaasdevops git repo
+curl -LO https://raw.githubusercontent.com/phdsaasdevops/MB2020_Script_ContainerRuntime_K8s_Deployment/master/CRI-O/1.15/crio.conf
+mv crio.conf /etc/crio/crio.conf
+
+#issue with crio networking https://github.com/cri-o/cri-o/issues/2411#issuecomment-540006558
+rm -rf /etc/cni/net.d/*
+
+systemctl stop crio
+systemctl start crio
+systemctl enable crio
+
+
+#install crictl 1.17.0
+VERSION="v1.17.0"
+curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-${VERSION}-linux-amd64.tar.gz --output crictl-${VERSION}-linux-amd64.tar.gz
+tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
+crictl info
+
+
+#install kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubeadm=1.15.12-00 kubelet=1.15.12-00 kubectl=1.15.12-00 kubernetes-cni
+systemctl enable kubelet
+#https://github.com/kidlj/kube/blob/master/README.md
+swapoff -a
+ 
+ 
+#initialize cluster
+kubeadm init --cri-socket=unix:///var/run/crio/crio.sock --apiserver-bind-port=443
+
+ 
+#configure kubectl
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+ 
+
+#Taint the master node to be able to deploy coredns pods properly
+kubectl taint nodes --all node-role.kubernetes.io/master-
+ 
+
+#install Weave Pod Network
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+ 
+ 
+#install helm
+curl -O https://get.helm.sh/helm-v3.0.1-linux-amd64.tar.gz
+tar -zxvf helm-v3.0.1-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/bin/
